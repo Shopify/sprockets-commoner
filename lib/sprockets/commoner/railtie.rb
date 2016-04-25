c6f905e958d7adba877567bdbd22d19738489dac
Bouke van der Bijl <boukevanderbijl@gmail.com>
2016-04-25 14:59:48 -0400

    Improve README

--- lib/sprockets/commoner/railtie.rb
+++ lib/sprockets/commoner/railtie.rb
@@ -2,6 +2,7 @@ module Sprockets
   module Commoner
     class Railtie < ::Rails::Railtie
       initializer 'sprockets-commoner' do
+        # We need to disable debugging because otherwise Rails will include each file individually, while we need everything to be bundled up together into a single file.
         config.assets.debug = false
         config.assets.paths << 'node_modules'
       end
