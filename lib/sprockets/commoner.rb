6a941e7128ae79b3c7592616103b3f0072aec5e2
Bouke van der Bijl <boukevanderbijl@gmail.com>
2016-05-04 13:22:33 -0400

    Add support for requiring JSON files

--- lib/sprockets/commoner.rb
+++ lib/sprockets/commoner.rb
@@ -1,4 +1,5 @@
 require 'sprockets'
+require 'sprockets/commoner/json_processor'
 require 'sprockets/commoner/processor'
 require 'sprockets/commoner/bundle'
 
@@ -7,6 +8,7 @@ module Sprockets
   end
 
   register_postprocessor 'application/javascript', ::Sprockets::Commoner::Processor
+  register_transformer 'application/json', 'application/javascript', ::Sprockets::Commoner::JSONProcessor
   register_bundle_metadata_reducer 'application/javascript', :commoner_enabled, false, :|
   register_bundle_metadata_reducer 'application/javascript', :commoner_used_helpers, Set.new, :+
   register_bundle_processor 'application/javascript', ::Sprockets::Commoner::Bundle
---
+++ test/json_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class JSONTest < MiniTest::Test
+  def setup
+    @env = Sprockets::Environment.new(File.join(__dir__, 'fixtures'))
+    @env.append_path File.join(__dir__, 'fixtures')
+  end
+
+  def test_import_json
+    assert asset = @env['import-json.js']
+    assert_equal <<-JS.chomp, asset.to_s.chomp
+!function() {
+var __commoner_initialize_module__ = function(f) {
+  var module = {exports: {}};
+  f.call(module.exports, module, module.exports);
+  return module.exports;
+};
+var global = window;
+
+var __commoner_module__import_json$data_json = __commoner_initialize_module__(function (module, exports) {
+  module.exports = {
+    "extra_info": 123
+  };
+});
+var __commoner_module__import_json$index_js = __commoner_initialize_module__(function (module, exports) {
+  console.log(__commoner_module__import_json$data_json.extra_info);
+});
+}();
+JS
+  end
+end
