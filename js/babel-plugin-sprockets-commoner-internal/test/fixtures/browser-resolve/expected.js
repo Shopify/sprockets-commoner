cb7a2171988c8e80f8c7d480cf1a0eec26344e47
Bouke van der Bijl <boukevanderbijl@gmail.com>
2016-05-06 14:05:51 -0400

    If a browser field makes browser-resolve ignore an import, replace with {}

--- js/babel-plugin-sprockets-commoner-internal/test/fixtures/browser-resolve/expected.js
+++ js/babel-plugin-sprockets-commoner-internal/test/fixtures/browser-resolve/expected.js
@@ -3,6 +3,10 @@ var __commoner_module__actual_js = __commoner_initialize_module__(function (modu
 
   var _shimmed2 = _interopRequireDefault(__commoner_module__shimmed_browser_js);
 
+  var _ignored = {};
+
+  var _ignored2 = _interopRequireDefault(_ignored);
+
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
   (0, _shimmed2.default)();
